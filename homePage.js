4a847da2792368c8dafa15b96ad3a41824bd6fef
Gons <gonza_af7@gmail.com>
2024-07-06 10:23:32 -0600

    grid layout

--- homePage.js
+++ homePage.js
@@ -2,7 +2,7 @@ const imageCarrousel = document.getElementById('image-carrousel');
 const buttonPrev = document.getElementById('prev');
 const buttonNext = document.getElementById('next');
 
-const imgArray = ['fuente.JPEG','local.JPEG']
+const imgArray = ['fuente.JPEG','local.JPEG','pin.JPEG','vistaCalle.JPEG']
 let currentImg = 0
 
 const showImage= (index) => {
@@ -10,7 +10,7 @@ const showImage= (index) => {
 };
 
 buttonNext.addEventListener('click',()=>{
-    currentImg = (currentImg + 1) % imgArray.length
+    currentImg = (currentImg + 1) % imgArray.length; /*modulo arithmetic approach*/
     showImage(currentImg)
     
 })
